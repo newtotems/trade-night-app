3504e097bc3d08ca9d39cd5d64ea82cfa57c9c3d
david-hogg <71979980+newtotems@users.noreply.github.com>
2024-08-25 12:06:49 +0100

    name

--- _data/eventlist.js
+++ _data/eventlist.js
@@ -18,13 +18,26 @@ module.exports = async function() {
             )
           )
         ),
-        q.Lambda('ref', q.Get(q.Var('ref')))
+        q.Lambda('ref', 
+          q.Let(
+            {
+              event: q.Get(q.Var('ref')),
+              eventType: q.Get(q.Ref(q.Collection('eventTypes'), q.Select(['data', 'eventTypeId'], q.Var('event'))))
+            },
+            {
+              id: q.Select(['ref', 'id'], q.Var('event')),
+              name: q.Select(['data', 'name'], q.Var('eventType')),
+              data: q.Select(['data'], q.Var('event'))
+            }
+          )
+        )
       )
     );
 
-    return result.data.map(event => ({
-      id: event.ref.id,
-      ...event.data
+    return result.data.map(item => ({
+      id: item.id,
+      name: item.name,
+      ...item.data
     }));
   } catch (error) {
     console.error('Error fetching events:', error);
