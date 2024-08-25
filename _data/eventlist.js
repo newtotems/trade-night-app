6e9c75bf36474028e13185f430dd1d611a66df9e
david-hogg <71979980+newtotems@users.noreply.github.com>
2024-08-25 12:14:19 +0100

    Update eventlist.js

--- _data/eventlist.js
+++ _data/eventlist.js
@@ -18,12 +18,35 @@ module.exports = async function() {
             )
           )
         ),
-        q.Lambda('ref', q.Get(q.Var('ref')))
+        q.Lambda(
+          'ref',
+          q.Let(
+            {
+              event: q.Get(q.Var('ref')),
+              eventType: q.Let(
+                {
+                  typeRef: q.Ref(q.Collection('eventTypes'), q.Select(['data', 'eventTypeId'], q.Var('event'), null))
+                },
+                q.If(
+                  q.And(q.IsRef(q.Var('typeRef')), q.Exists(q.Var('typeRef'))),
+                  q.Get(q.Var('typeRef')),
+                  null
+                )
+              )
+            },
+            {
+              id: q.Select(['ref', 'id'], q.Var('event')),
+              eventTypeName: q.Select(['data', 'name'], q.Var('eventType'), ''),
+              data: q.Select(['data'], q.Var('event'))
+            }
+          )
+        )
       )
     );
 
     return result.data.map(event => ({
-      id: event.ref.id,
+      id: event.id,
+      eventTypeName: event.eventTypeName,
       ...event.data
     }));
   } catch (error) {
